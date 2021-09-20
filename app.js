9a972ab63727c613a601e72bb596af56e9a0d87a
hyunjungagnes <hyunjung.agnes.oh@gmail.com>
2021-09-20 10:48:59 +0800

    hello

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 
 const app = express();
 app.use(cors());
+app.use(express.static(path.join(__dirname, 'public')));
 
 const getFullUrl = (req) =>
   `${req.protocol}://${req.headers.host}${req.originalUrl}`;
